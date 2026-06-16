3c1d8d0da674780a0554aa2497ca849715608ed2
Arya Ghayedi <aryaghayedi@gmail.com>
2026-06-16 19:20:03 +0330

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Main from './components/Main'
+import Header from './components/Header'
+
+
+function App() {
+
+  return (
+    <>
+     <Header />
+     <Main />
+    </>
+  )
+}
+
+export default App
