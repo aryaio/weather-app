115eb23c2b7832681bb6683c22dbf04f1e8931bc
Arya Ghayedi <aryaghayedi@gmail.com>
2026-06-16 19:22:53 +0330

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header(){
+    return(
+        <>
+            <header>
+                <h4>Weather Now</h4>
+                <button className="unit-btn">Units</button>
+            </header>
+        </>
+    )
+}
+
+export default Header
